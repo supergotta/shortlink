6a261c31515518a4b84ef943a0429c944d69db82
supergotta <1615507473@qq.com>
2023-11-26 22:00:41 +0800

    feature: 开发短链接后管系统-通过分布式锁完善用户注册功能, 防止短时间内过多恶意请求打进数据库, 使用统一未注册用户名注册新用户

---
+++ admin/src/main/java/com/supergotta/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,9 @@
+package com.supergotta.shortlink.admin.common.constant;
+
+/**
+ * 短链接后管Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    public static final String LOCK_USER_REGISTER_KEY = "lock:";
+}
--- admin/src/main/java/com/supergotta/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/supergotta/shortlink/admin/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.supergotta.shortlink.admin.common.constant.RedisCacheConstant;
 import com.supergotta.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.supergotta.shortlink.admin.common.exception.ClientException;
 import com.supergotta.shortlink.admin.dao.entity.UserDO;
@@ -13,6 +14,8 @@ import com.supergotta.shortlink.admin.dto.resp.UserRespDTO;
 import com.supergotta.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +27,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -58,12 +62,25 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (!hasUsername(requestParam.getUsername())){
             throw new ClientException(UserErrorCodeEnum.USERNAME_EXISTED);
         }
-        // 新增用户
-        int insertCount = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if (insertCount < 1){
-            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+
+        // 获取Redisson分布式锁
+        String key = RedisCacheConstant.LOCK_USER_REGISTER_KEY + requestParam.getUsername();
+        RLock lock = redissonClient.getLock(key);
+
+        // 尝试获取锁
+        try {
+            if (lock.tryLock()){
+                // 新增用户
+                int insertCount = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if (insertCount < 1){
+                    throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                }
+                // 将username插入到布隆过滤器中
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+            }
+            throw new ClientException(UserErrorCodeEnum.USERNAME_EXISTED);
+        }finally {
+            lock.unlock();
         }
-        // 将username插入到布隆过滤器中
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
